601f9f39ff07ae533c44ed38f70280281e769023
komori <komori@catchup.co.jp>
2022-02-10 00:27:58 +0900

    完了

---
+++ index.js
@@ -0,0 +1,16 @@
+function change1() {
+    text1.style.fontFamily = "'M PLUS Rounded 1c', sans-serif";
+}
+function change2() {
+    text2.style.fontFamily = "'Syne Tactile', cursive";
+}
+function change3() {
+    text3.style.fontFamily = "'Festive', cursive";
+}
+function change4() {
+    text4.style.fontFamily = "'Bebas Neue', cursive";
+}
+function change5() {
+    text5.style.fontFamily = "'Dancing Script', cursive";
+}
+
